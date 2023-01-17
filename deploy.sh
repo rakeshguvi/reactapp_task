f087b741b3e49bc72bc85d77abee87a671be73ff
rakeshguvi <114785421+rakeshguvi@users.noreply.github.com>
2023-01-18 01:05:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-image_id=$(docker images | grep 'image_name' | awk '{print $3}')
+image_id=$(docker images | grep 'rakeshguvi/react_' | awk '{print $1}')
 if [ "$image_id" == "rakeshguvi/react_dev" ]; then  
       docker push rakeshguvi/react_dev:v1
 elif [ "$image_id" == "rakeshguvi/react_prod" ]; then
