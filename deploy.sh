a4c32cc08ab81b7918eb99e3d0e39200bf5b8c05
rakeshguvi <114785421+rakeshguvi@users.noreply.github.com>
2023-01-18 00:58:30 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
-docker-compose up -d
-docker ps
+image_id=$(docker images | grep 'image_name' | awk '{print $3}')
+if [ "$image_id" == "rakeshguvi/react_dev" ]; then  
+      docker push rakeshguvi/react_dev:v1
+elif [ "$image_id" == "rakeshguvi/react_prod" ]; then
+      docker push rakeshguvi/react_prod:v1
+else
+    echo " No Image"
+fi
