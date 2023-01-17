522e1022867fc08648c48e0d5772e9e341e95078
rakeshguvi <114785421+rakeshguvi@users.noreply.github.com>
2023-01-17 23:36:44 +0530

    Update branch.sh

--- branch.sh
+++ branch.sh
@@ -6,6 +6,6 @@ git_branch=$(git rev-parse --abbrev-ref HEAD)
 # Check the current git branch
 if [ "$git_branch" == "dev" ]; then
     git checkout dev
-else [ "$git_branch" == "main" ]; then
+else [ "$git_branch" == "main" ];
     git checkout main
 fi
