59f8f2e927b6d41917477aadb686ca0f0456b481
rakeshguvi <114785421+rakeshguvi@users.noreply.github.com>
2023-01-18 00:10:11 +0530

    Update branch.sh

--- branch.sh
+++ branch.sh
@@ -5,7 +5,7 @@ git_branch=$(git rev-parse --abbrev-ref HEAD)
 
 # Check the current git branch
 if [ "$git_branch" == "dev" ]; then
-    git checkout dev
+    docker build -t rakeshguvi/react_dev:v1 
 else [ "$git_branch" == "main" ];
     git checkout main
 fi
