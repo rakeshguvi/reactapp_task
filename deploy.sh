47e95427931d356d3edc6f628aec220e31c7b00a
rakeshguvi <rakeshguvi@gmail.com>
2023-01-18 01:58:21 +0530

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
-docker-compose up -d
-docker ps
+image_id=$(docker images | grep 'rakeshguvi/react_' | awk '{print $1}')
+if [ "$image_id" == "rakeshguvi/react_dev" ]; then  
+      docker push rakeshguvi/react_dev:v1
+elif [ "$image_id" == "rakeshguvi/react_prod" ]; then
+        docker push rakeshguvi/react_prod:v1
+else
+        echo " No Image"
+fi
