f667739154bf71b6e475c622ddf6abca4633f0a1
rakeshguvi <114785421+rakeshguvi@users.noreply.github.com>
2023-01-17 23:31:32 +0530

    file added

---
+++ branch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the current git branch
+git_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# Check the current git branch
+if [ "$git_branch" == "dev" ]; then
+    git checkout dev
+else [ "$git_branch" == "main" ]; then
+    git checkout main
+fi
