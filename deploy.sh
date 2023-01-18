befe8843c231836e7e51f21e0ec21a8ee672e9a3
rakeshguvi <rakeshguvi@gmail.com>
2023-01-18 20:23:27 +0530

    docker-cred

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ image_id=$(docker images | grep 'rakeshguvi/react_' | awk '{print $1}')
 if [ "$image_id" == "rakeshguvi/react_dev" ]; then  
       docker push rakeshguvi/react_dev:v1
 elif [ "$image_id" == "rakeshguvi/react_prod" ]; then
-            docker push rakeshguvi/react_prod:v1
+      docker push rakeshguvi/react_prod:v1
 else
-        echo " No Image"
+      echo " No Image"
 fi
