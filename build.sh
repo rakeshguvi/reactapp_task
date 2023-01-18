80f629568b38f3fda2803f276d0c04b5d37d004c
rakeshguvi <rakeshguvi@gmail.com>
2023-01-18 13:07:06 +0530

    build7

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ git_branch=$(git rev-parse --abbrev-ref HEAD)
 #git_branch=$(ls | grep package.json)
 # Check the current git branch
 if [ "$git_branch" == "dev" ]; then
-    docker build -t --no-cache rakeshguvi/react_dev:$(docker images | awk '($1 == "rakeshguvi/react_dev") {print $2 += .01; exit}') .
-else [ "$git_branch" == "main" ];
-      docker build -t --no-cache rakeshguvi/react_prod:$(docker images | awk '($1 == "rakeshguvi/react_prod") {print $2 += .01; exit}') .
+	docker build -t rakeshguvi/react_dev:v1 .
+else
+	docker build -t rakeshguvi/react_prod:v1 .
 fi
 #if [ "$git_branch" == "package.json" ]; then
 #	docker build -t rakeshguvi/react_prod:v1 .
