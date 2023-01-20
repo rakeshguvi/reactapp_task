ca1b9a1c5efc9e853c7e742cfb6456329230c587
rakeshguvi <rakeshguvi@gmail.com>
2023-01-20 07:08:34 +0530

    deploy1

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,13 @@
+#!/bin/sh
+
+# Set the registry URL and access token
+REGISTRY=docker.io
+TOKEN=dckr_pat_VPk80pOTPVgQaQLfmuHGc82od6U
+
+# Log in to the registry using the token
+echo $TOKEN | docker login $REGISTRY -u rakeshguvi --password-stdin
+
+#Push the image
 image_id=$(docker images | grep 'rakeshguvi/react_' | awk '{print $1}')
 if [ "$image_id" == "rakeshguvi/react_dev" ]; then  
       docker push rakeshguvi/react_dev:v1
